bdb4288dd4cb449ab4303cf709983d315ebb6ea7
ion <scorobogaciion@gmail.com>
2023-04-09 19:06:44 +0300

    Added getPost resolver

--- iac/lib/app-sync-stack.ts
+++ iac/lib/app-sync-stack.ts
@@ -52,14 +52,31 @@ export class AppSyncStack extends cdk.Stack {
             runtime: FunctionRuntime.JS_1_0_0,
         });
 
+        const getPostFunction = new AppsyncFunction(this, 'GetPostFunction', {
+            name: 'GET_POST',
+            api: appsyncApi,
+            dataSource: postsDataSource,
+            code: Code.fromAsset(path.join(__dirname, './../templates/functions/getPost.js')),
+            runtime: FunctionRuntime.JS_1_0_0,
+        });
 
-        new Resolver(this, 'AddPostPipelineResolver', {
+
+        new Resolver(this, 'AddPostResolver', {
             api: appsyncApi,
             typeName: 'Mutation',
             fieldName: 'addPost',
             code: Code.fromAsset(path.join(__dirname, './../templates/functions/pipeline.js')),
             runtime: FunctionRuntime.JS_1_0_0,
             pipelineConfig: [addPostFunction],
+        });
+
+        new Resolver(this, 'GetPostResolver', {
+            api: appsyncApi,
+            typeName: 'Query',
+            fieldName: 'getPost',
+            code: Code.fromAsset(path.join(__dirname, './../templates/functions/pipeline.js')),
+            runtime: FunctionRuntime.JS_1_0_0,
+            pipelineConfig: [getPostFunction],
         });
 
     }
