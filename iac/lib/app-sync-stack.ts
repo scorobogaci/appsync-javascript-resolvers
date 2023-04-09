8638c930264999a71afe40f48935983c1d1086f2
ion <scorobogaciion@gmail.com>
2023-04-09 19:01:09 +0300

    Configured pipeline resolver

--- iac/lib/app-sync-stack.ts
+++ iac/lib/app-sync-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {StackProps} from "aws-cdk-lib";
-import {AuthorizationType, GraphqlApi, SchemaFile} from "aws-cdk-lib/aws-appsync";
+import {
+    AppsyncFunction,
+    AuthorizationType, Code, FunctionRuntime,
+    GraphqlApi, Resolver,
+    SchemaFile,
+} from "aws-cdk-lib/aws-appsync";
 import * as path from "path";
 import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb";
 
@@ -25,6 +30,7 @@ export class AppSyncStack extends cdk.Stack {
 
         const postsTable = new Table(this, 'DemoTable', {
             tableName: props.stage.concat('-posts'),
+            deletionProtection: false,
             partitionKey: {
                 name: 'id',
                 type: AttributeType.STRING,
@@ -37,5 +43,24 @@ export class AppSyncStack extends cdk.Stack {
         })
 
         const postsDataSource = appsyncApi.addDynamoDbDataSource('postsDataSource', postsTable);
+
+        const addPostFunction = new AppsyncFunction(this, 'AddPostFunction', {
+            name: 'ADD_POST',
+            api: appsyncApi,
+            dataSource: postsDataSource,
+            code: Code.fromAsset(path.join(__dirname, './../templates/functions/addPost.js')),
+            runtime: FunctionRuntime.JS_1_0_0,
+        });
+
+
+        new Resolver(this, 'AddPostPipelineResolver', {
+            api: appsyncApi,
+            typeName: 'Mutation',
+            fieldName: 'addPost',
+            code: Code.fromAsset(path.join(__dirname, './../templates/functions/pipeline.js')),
+            runtime: FunctionRuntime.JS_1_0_0,
+            pipelineConfig: [addPostFunction],
+        });
+
     }
 }
